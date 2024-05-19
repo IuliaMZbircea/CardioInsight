69e79c059a2debca0a825a5c0cd2b0a5d82970e0
iuliazbircea <iuliazbircea@gmail.com>
2024-05-19 14:09:11 +0300

    Remove unused files and folders

--- Cardio-Insight-iOS/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-// initialize configuration
-const config = getDefaultConfig(__dirname);
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-
-module.exports = config;
